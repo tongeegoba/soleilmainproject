fd33837d43ca2338307285b77161fcc3c5e6001a
goba <tonygeegoba@gmail.com>
2019-01-27 17:57:53 +0200

    production settings whitenoise

--- main/settings.py
+++ main/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -127,10 +128,12 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-     os.path.join(BASE_DIR, 'static_storage')]
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [
+     os.path.join(BASE_DIR, 'STATIC')]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static-serve')
 
 LOGIN_REDIRECT_URL = '/articles/list/'
 
